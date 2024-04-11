5fd743e9a210c8b2b673ab94408b8a2b1ebccb50
ssh00n <seunghunshin1284@gmail.com>
2024-04-11 20:28:15 +0900

    Chore: 프로젝트 이름 변경

--- src/main/java/com/mansour/ide/IdeApplication.java
+++ src/main/java/com/mansour/ide/IdeApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.ide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class IdeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(IdeApplication.class, args);
 	}
 
 }
