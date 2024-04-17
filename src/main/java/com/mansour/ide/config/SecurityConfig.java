130590b010d471f2a8e2fa18e8092a77f3e76e6e
ssh00n <seunghunshin1284@gmail.com>
2024-04-18 02:09:35 +0900

    Fix: Lazy annotation to resolve circular dependency

--- src/main/java/com/mansour/ide/config/SecurityConfig.java
+++ src/main/java/com/mansour/ide/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.mansour.ide.member.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,6 +46,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Lazy
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
