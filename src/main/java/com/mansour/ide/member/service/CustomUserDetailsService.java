3b5d0b392f52e6845095ddf94218d124af3dbaa3
ssh00n <seunghunshin1284@gmail.com>
2024-04-18 01:56:14 +0900

    Feat: Member 도메인 회원가입/로그인/로그아웃/회원탈퇴 구현

---
+++ src/main/java/com/mansour/ide/member/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.mansour.ide.member.service;
+
+
+import com.mansour.ide.member.model.Member;
+import com.mansour.ide.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByLoginId(username);
+        if (member == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return User.builder()
+                .username(member.getLoginId())
+                .password(member.getPassword())
+                .roles("USER") // 기본 역할
+                .build();
+    }
+}
+
