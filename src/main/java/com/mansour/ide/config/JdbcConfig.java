e848424346c96143754c4086d6d30308b8dbbb88
ssh00n <seunghunshin1284@gmail.com>
2024-04-18 01:55:48 +0900

    Feat: Config Module - Spring Security, Jdbc Config

---
+++ src/main/java/com/mansour/ide/config/JdbcConfig.java
@@ -0,0 +1,14 @@
+package com.mansour.ide.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfig {
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
