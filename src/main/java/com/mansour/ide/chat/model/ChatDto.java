77a313aa3746f6c149a7f42ba19f09be5c66893d
toughCircle <toughCircle@gmail.com>
2024-04-19 15:34:28 +0900

    Feat: 채팅 기능

--- src/main/java/com/mansour/ide/chat/model/ChatDto.java
+++ src/main/java/com/mansour/ide/chat/model/ChatDto.java
@@ -10,13 +10,13 @@ import java.time.LocalDateTime;
 @ToString
 public class ChatDto {
 
-    private Long chatMessageId;
-    private Long userId;
-    private Long projectId;
-    private String message;
-    private LocalDateTime createAt;
-    private String sender;
-    private MessageType messageType;
+    private Long chatMessageId; // 메시지 id
+    private Long userId; // 보내는 유저 id
+    private Long projectId; // 프로젝트 id
+    private String message; // 메시지 내용
+    private LocalDateTime createAt; // 보낸 시간
+    private String sender; // 보낸 유저 닉네임
+    private MessageType messageType; // 메시지 타입
 
     public enum MessageType {
         JOIN, LEAVE, TALK
