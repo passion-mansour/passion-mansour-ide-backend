249c25da852bc9fa45dd4913c0d37ede5e0d6c48
ssh00n <seunghunshin1284@gmail.com>
2024-04-21 20:18:00 +0900

    Feat: Spring Security Principal Details

---
+++ src/main/java/com/mansour/ide/common/security/JwtTokenUtil.java
@@ -0,0 +1,69 @@
+package com.mansour.ide.common.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenUtil {
+    private final String secret = "oyET2R4Mh7TV8FlYcSzcnucv5nh4TJnarWf0btpLZpN/CqGnyM/zhvONr2cpUrBeROlB/LtW8a9SYz2QfS22tw==";  // 비밀키 설정
+    private long accessTokenValidity = 24 * 60 * 60 * 1000;
+    private long refreshTokenValidity = 7 * 24 * 60 * 60 * 1000;
+
+    private Key getKey() {
+        return new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+    }
+
+    // 토큰에서 사용자 이름 추출
+    public String getUsernameFromToken(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    public String generateAccessToken(UserDetails userDetails) {
+        return generateToken(userDetails.getUsername(), accessTokenValidity);
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        return generateToken(userDetails.getUsername(), refreshTokenValidity);
+    }
+
+    // 토큰 생성 로직
+    private String generateToken(String subject, long validity) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + validity))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 토큰 유효성 검증
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    // 토큰에서 정보 추출
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    // 토큰의 만료일 확인
+    public Boolean isTokenExpired(String token) {
+        final Date expiration = getAllClaimsFromToken(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
+}
