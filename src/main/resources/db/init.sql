0387a3608645ca152d90a4ff361c2c3de82cdfe1
toughCircle <toughCircle@gmail.com>
2024-04-21 12:11:33 +0900

    Fix: sql 수정

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,37 +1,9 @@
 -- init.sql
 
 
-CREATE DATABASE IF NOT EXISTS database;
-USE database;
+CREATE DATABASE IF NOT EXISTS `database`;
+USE `database`;
 
-
-
-CREATE TABLE IF NOT EXISTS member (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    nickName VARCHAR(255),
-    loginId VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS messages (
-    message_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    project_id BIGINT,
-    participant_id BIGINT,
-    message VARCHAR(255),
-    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id) ON DELETE CASCADE,
-    FOREIGN KEY (participant_id) REFERENCES Participant(participant_id)
-);
-
-CREATE TABLE IF NOT EXISTS participant (
-    participantId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    projectId BIGINT,
-    userId BIGINT,
-    permission BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (projectId) REFERENCES Projects(projectId),
-    FOREIGN KEY (userId) REFERENCES Member(id)
-);
 
 -- Projects 테이블 생성
 CREATE TABLE IF NOT EXISTS projects (
@@ -43,10 +15,37 @@ CREATE TABLE IF NOT EXISTS projects (
     maxUser INT,
     language VARCHAR(255),
     isEnd BOOLEAN NOT NULL DEFAULT FALSE
-)
+);
+
+CREATE TABLE IF NOT EXISTS member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    nickName VARCHAR(255),
+    loginId VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS participant (
+    participant_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    project_id BIGINT,
+    user_id BIGINT,
+    permission BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (project_id) REFERENCES projects(project_id),
+    FOREIGN KEY (user_id) REFERENCES member(id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    message_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    project_id BIGINT,
+    participant_id BIGINT,
+    message VARCHAR(255),
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
+    FOREIGN KEY (participant_id) REFERENCES participant(participant_id)
+);
 
 
 CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON db.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON `database`.* TO 'admin'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
