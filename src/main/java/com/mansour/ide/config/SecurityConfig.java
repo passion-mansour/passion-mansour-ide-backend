0e9de9d7649f712c83bcd186cdd9f92085828e59
ssh00n <seunghunshin1284@gmail.com>
2024-04-23 23:41:33 +0900

    Fix: Resolve conflict mismatch

--- src/main/java/com/mansour/ide/config/SecurityConfig.java
+++ src/main/java/com/mansour/ide/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig {
-
+    //
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
