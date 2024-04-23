d815f25cd8352dcec840b1ec31b2fcf888d3cae5
toughCircle <toughCircle@gmail.com>
2024-04-24 08:37:44 +0900

    Fix: sql 수정

--- docker/init.sql
+++ docker/init.sql
@@ -13,43 +13,32 @@ FLUSH PRIVILEGES;
 CREATE TABLE IF NOT EXISTS member (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    nickname VARCHAR(255),
-    login_id VARCHAR(255) NOT NULL UNIQUE,
+    nickName VARCHAR(255),
+    loginId VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
 -- Ensure `project` table is created before `participant` and `message` tables because it's referenced by them
 CREATE TABLE IF NOT EXISTS project (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    host_id BIGINT,
+    hostId BIGINT,
     pw VARCHAR(255),
     title VARCHAR(255),
-    tag_language VARCHAR(255),
-    max_user INT,
-    is_lock BOOLEAN,
-    is_end BOOLEAN,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    end_at TIMESTAMP,
+    tagLanguage VARCHAR(255),
+    maxUser INT,
+    isLock BOOLEAN,
+    isEnd BOOLEAN,
+    createdDt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    endDt TIMESTAMP,
     fileId BIGINT
 );
 
-CREATE TABLE IF NOT EXISTS participant (
-    participant_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    project_id BIGINT,
-    user_id BIGINT,
-    permission BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (project_id) REFERENCES project(id),
-    FOREIGN KEY (user_id) REFERENCES member(id)
-);
-
 CREATE TABLE IF NOT EXISTS message (
-    message_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    project_id BIGINT,
-    participant_id BIGINT,
+    messageId BIGINT AUTO_INCREMENT PRIMARY KEY,
+    projectId BIGINT,
+    userId BIGINT,
     message VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE,
-    FOREIGN KEY (participant_id) REFERENCES participant(participant_id) ON DELETE CASCADE
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS file (
@@ -57,8 +46,8 @@ CREATE TABLE IF NOT EXISTS file (
     name VARCHAR(255),
     content VARCHAR(255),
     language VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP
+    createdDt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedDt TIMESTAMP
 );
 
 
