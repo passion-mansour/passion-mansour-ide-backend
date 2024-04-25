a379edeb75b945190636fda8fcaf487497b46b70
toughCircle <toughCircle@gmail.com>
2024-04-25 23:21:24 +0900

    저장 로직 제거

--- src/main/java/com/mansour/ide/chat/repository/ChatRepository.java
+++ src/main/java/com/mansour/ide/chat/repository/ChatRepository.java
@@ -19,30 +19,30 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class ChatRepository {
 
-    private final NamedParameterJdbcTemplate template;
-
-    private RowMapper<Message> rowMapper() {
-        return BeanPropertyRowMapper.newInstance(Message.class);
-    }
-
-    public Message save(Message message) {
-        final String sql = "INSERT INTO message (projectId, userId, message, createdAt) " +
-            "VALUES (:projectId, :userId, :message, :createdAt)";
-
-        MapSqlParameterSource parameter = new MapSqlParameterSource()
-            .addValue("projectId", message.getProjectId())
-            .addValue("userId", message.getUserId())
-            .addValue("message", message.getMessage())
-            .addValue("createdAt", message.getCreatedAt());
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        template.update(sql, parameter, keyHolder);
-
-        message.setMessageId(keyHolder.getKey().longValue());
-
-        log.info("chatMessage {}", message);
-        return message;
-    }
+//    private final NamedParameterJdbcTemplate template;
+//
+//    private RowMapper<Message> rowMapper() {
+//        return BeanPropertyRowMapper.newInstance(Message.class);
+//    }
+//
+//    public Message save(Message message) {
+//        final String sql = "INSERT INTO message (projectId, userId, message, createdAt) " +
+//            "VALUES (:projectId, :userId, :message, :createdAt)";
+//
+//        MapSqlParameterSource parameter = new MapSqlParameterSource()
+//            .addValue("projectId", message.getProjectId())
+//            .addValue("userId", message.getUserId())
+//            .addValue("message", message.getMessage())
+//            .addValue("createdAt", message.getCreatedAt());
+//
+//        KeyHolder keyHolder = new GeneratedKeyHolder();
+//        template.update(sql, parameter, keyHolder);
+//
+//        message.setMessageId(keyHolder.getKey().longValue());
+//
+//        log.info("chatMessage {}", message);
+//        return message;
+//    }
 
     // TODO: 검색기능
 }
--- src/main/java/com/mansour/ide/chat/service/ChatService.java
+++ src/main/java/com/mansour/ide/chat/service/ChatService.java
@@ -25,46 +25,46 @@ public class ChatService {
     private final ChatRepository chatRepository;
     private final MemberRepository memberRepository;
 
-    @Transactional
-    public ChatDto saveMessage(Long projectId, ChatDto chatDto) {
-
-        Message message = new Message();
-        message.setProjectId(projectId);
-        message.setUserId(chatDto.getUserId());
-        message.setMessage(chatDto.getMessage());
-
-        if (chatDto == null || chatDto.getTimestamp() == null) {
-            // 로그를 남기거나, 예외를 던지거나, 기본값을 사용합니다.
-            log.error("Received null message or timestamp");
-            LocalDateTime localDateTime = LocalDateTime.now();
-            message.setCreatedAt(localDateTime);
-        }
-        try {
-            ZonedDateTime dateTime = ZonedDateTime.parse(chatDto.getTimestamp());
-            LocalDateTime localDateTime = dateTime.toLocalDateTime();
-            message.setCreatedAt(localDateTime);
-        } catch (DateTimeParseException e) {
-            log.error("Failed to parse timestamp: " + chatDto.getTimestamp(), e);
-        }
-
-        Message saved = chatRepository.save(message);
-
-        return convertToDto(saved);
-    }
-
-    private ChatDto convertToDto(Message message) {
-
-        Member findUser = memberRepository.findById(message.getUserId());
-
-        ChatDto chatDto = new ChatDto();
-        chatDto.setSender(findUser.getNickName());
-        chatDto.setChatMessageId(message.getMessageId());
-        chatDto.setMessage(chatDto.getMessage());
-        chatDto.setProjectId(message.getProjectId());
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-        chatDto.setTimestamp(message.getCreatedAt().format(formatter));
-
-        return chatDto;
-    }
+//    @Transactional
+//    public ChatDto saveMessage(Long projectId, ChatDto chatDto) {
+//
+//        Message message = new Message();
+//        message.setProjectId(projectId);
+//        message.setUserId(chatDto.getUserId());
+//        message.setMessage(chatDto.getMessage());
+//
+//        if (chatDto == null || chatDto.getTimestamp() == null) {
+//            // 로그를 남기거나, 예외를 던지거나, 기본값을 사용합니다.
+//            log.error("Received null message or timestamp");
+//            LocalDateTime localDateTime = LocalDateTime.now();
+//            message.setCreatedAt(localDateTime);
+//        }
+//        try {
+//            ZonedDateTime dateTime = ZonedDateTime.parse(chatDto.getTimestamp());
+//            LocalDateTime localDateTime = dateTime.toLocalDateTime();
+//            message.setCreatedAt(localDateTime);
+//        } catch (DateTimeParseException e) {
+//            log.error("Failed to parse timestamp: " + chatDto.getTimestamp(), e);
+//        }
+//
+//        Message saved = chatRepository.save(message);
+//
+//        return convertToDto(saved);
+//    }
+//
+//    private ChatDto convertToDto(Message message) {
+//
+//        Member findUser = memberRepository.findById(message.getUserId());
+//
+//        ChatDto chatDto = new ChatDto();
+//        chatDto.setSender(findUser.getNickName());
+//        chatDto.setChatMessageId(message.getMessageId());
+//        chatDto.setMessage(chatDto.getMessage());
+//        chatDto.setProjectId(message.getProjectId());
+//        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+//        chatDto.setTimestamp(message.getCreatedAt().format(formatter));
+//
+//        return chatDto;
+//    }
 
 }
