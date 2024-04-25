7fc8cd5bc9c148770fea1d86ddb30f8b57193c29
toughCircle <toughCircle@gmail.com>
2024-04-25 16:14:48 +0900

    Fix: 프론트 배포 주소 수정

--- src/main/java/com/mansour/ide/config/WebSocketConfig.java
+++ src/main/java/com/mansour/ide/config/WebSocketConfig.java
@@ -13,6 +13,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
