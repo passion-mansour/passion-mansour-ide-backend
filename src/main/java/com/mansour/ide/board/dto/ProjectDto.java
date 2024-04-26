73d8f859ea24dc78bf738af4d6e181bd357e1b1d
toughCircle <toughCircle@gmail.com>
2024-04-26 10:15:02 +0900

    Feat: 프로젝트 입장, 퇴장 소켓 관련 기능 추가

---
+++ src/main/java/com/mansour/ide/board/dto/ProjectDto.java
@@ -0,0 +1,22 @@
+package com.mansour.ide.board.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class ProjectDto {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long userId;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private int maxUser;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String type;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Boolean isOwn;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+}
